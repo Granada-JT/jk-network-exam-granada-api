9f6beef86ec98fc468f5540a9aaa0c512c063a57
Jomar Tabios Granada <jomart.granada@gmail.com>
2026-02-05 17:49:12 +0800

    fix(dotenv): update env implementation
    - moved server.js to root for correct env initialization
    - updated package.json scripts
    - updated README quick start guide

--- prisma.config.js
+++ prisma.config.js
@@ -1,9 +1,4 @@
 import { defineConfig } from '@prisma/config';
-import { PrismaMariaDb } from '@prisma/adapter-mariadb';
-import mysql from 'mysql2/promise';
-import dotenv from 'dotenv';
-
-dotenv.config();
 
 const { DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME } = process.env;
 const connectionString = `mysql://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`;
